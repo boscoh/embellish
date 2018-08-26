5e9e60dcde53633df3dff0df8b7d99c9adfb9476
Bosco Ho <apposite@gmail.com>
2018-08-26 20:29:42 +1000

    fixed code blocks -> inadvertent double markdown parsing

--- embellish.js
+++ embellish.js
@@ -15,70 +15,65 @@ embellish - a static site generator
 usage: embellish [-s SITE.yaml] [-w] [-r] (markdown | directory)
 `;
 
-const fs = require('fs-extra');
-const path = require('path');
-const _ = require('lodash');
-const nopt = require('nopt');
-const slug = require('slug');
-const globule = require('globule');
+const fs = require("fs-extra");
+const path = require("path");
+const _ = require("lodash");
+const nopt = require("nopt");
+const slug = require("slug");
+const globule = require("globule");
 const pug = require("pug");
-const normalize = require('normalization');
-const yaml = require('js-yaml');
-const dateFormat = require('dateformat');
-const commonmark = require('commonmark');
-const yamlFront = require('yaml-front-matter');
+const normalize = require("normalization");
+const yaml = require("js-yaml");
+const dateFormat = require("dateformat");
+const yamlFront = require("yaml-front-matter");
 
+const commonmark = require("commonmark");
+let reader = new commonmark.Parser();
+let writer = new commonmark.HtmlRenderer();
 
 function isFile(f) {
   try {
     return fs.lstatSync(f).isFile();
-  }
-  catch (e) {
+  } catch (e) {
     return false;
   }
 }
-
 
 function isDirectory(file) {
   try {
     return fs.lstatSync(file).isDirectory();
-  }
-  catch (e) {
+  } catch (e) {
     return false;
   }
 }
 
-
 function pprint(name, obj) {
-  console.log(name, '=', JSON.stringify(obj, null, 2));
+  console.log(name, "=", JSON.stringify(obj, null, 2));
 }
-
 
 function stripExtOfPathname(filename) {
   let n = filename.length - path.extname(filename).length;
   return filename.substring(0, n);
 }
 
-
 function convertUnicodeCharsToHtml(str) {
-    let strLength = str.length;
-    if (strLength === 0) {
-        return '';
+  let strLength = str.length;
+  if (strLength === 0) {
+    return "";
+  }
+  let result = "";
+  let i = 0;
+  while (i < strLength) {
+    let c = str.charCodeAt(i);
+    if (c <= 127) {
+      result += str[i++];
+      continue;
     }
-    let result = '';
-    let i = 0;
-    while (i < strLength) {
-        let c = str.charCodeAt(i);
-        if (c <= 127) {
-            result += str[i++];
-            continue;
-        }
-        result += '&#' + c + ';';
-        i++
-    }
-    return result;
+    result += "&#" + c + ";";
+    i++;
+  }
+  return result;
 }
-
 
 function getFiles(dir, exts, recursive) {
   let files = [];
@@ -94,42 +89,36 @@ function getFiles(dir, exts, recursive) {
   return files;
 }
 
-
 function convertCommonmarkToHtml(text) {
-	let reader = new commonmark.Parser();
-	let writer = new commonmark.HtmlRenderer();
-	let parsed = reader.parse(text);
-	return convertUnicodeCharsToHtml(writer.render(parsed));
+  let html = writer.render(reader.parse(text))
+  return convertUnicodeCharsToHtml(html);
 }
-
 
 function getModifiedDate(file) {
   let stats = fs.statSync(file);
   return new Date(stats.mtime);
 }
 
-
 let defaultPage = {
-  template: 'default.pug',  // name of template file
-  filename: null,  // name of markdown file
-  checksum: '', // checksum used to check final output to avoid redundant writes
-  excerpt: '', // text to put in excerpt, no tags please!
-  content: '',  // main text of article
-  title: '',  // title for indexing and for large display
-  category: '',  // category of article for indexing
-  relSiteUrl: '',  // the top site directory relative to this page
+  template: "default.pug", // name of template file
+  filename: null, // name of markdown file
+  checksum: "", // checksum used to check final output to avoid redundant writes
+  excerpt: "", // text to put in excerpt, no tags please!
+  content: "", // main text of article
+  title: "", // title for indexing and for large display
+  category: "", // category of article for indexing
+  relSiteUrl: "", // the top site directory relative to this page
   dateFormatString: null, // dateFormat formatting string
-  date: null,  // published date
-  slug: null,  // url-safe name of article used to make url and files
-  url: '',   // relative url used for links in index files
-  target: '',    // target filename, maybe different to url due to redirection
-  index: false,   // indicates if this is an indexing page
-  sortKey: null,  // the field on which to sort under in indexing
-  sortReverse: true,  // ascending or descing order for sorting
-  subpages: [],   // in indexing, pages belonging to the index placed here
-  maxSubpages: null,  // a maximum limit of files to put in subpages
+  date: null, // published date
+  slug: null, // url-safe name of article used to make url and files
+  url: "", // relative url used for links in index files
+  target: "", // target filename, maybe different to url due to redirection
+  index: false, // indicates if this is an indexing page
+  sortKey: null, // the field on which to sort under in indexing
+  sortReverse: true, // ascending or descing order for sorting
+  subpages: [], // in indexing, pages belonging to the index placed here
+  maxSubpages: null // a maximum limit of files to put in subpages
 };
-
 
 function readPages(site) {
   let readExts = site.readExts;
@@ -150,7 +139,6 @@ function readPages(site) {
   let nSkip = 0;
   console.log(`readPages ${site.files.length}`);
   for (let file of site.files) {
-
     let page = _.cloneDeep(defaultPage);
     if (_.has(cachedPages, file)) {
       _.assign(page, cachedPages[file]);
@@ -159,8 +147,8 @@ function readPages(site) {
 
     let modified = getModifiedDate(file);
     if (Date.parse(modified) != Date.parse(page.modified)) {
-      _.assign(page, yamlFront.loadFront(file, 'content'));
-      page.content = convertCommonmarkToHtml(page.content)
+      _.assign(page, yamlFront.loadFront(file, "content"));
+      page.content = convertCommonmarkToHtml(page.content);
       page.modified = modified;
     } else {
       nSkip += 1;
@@ -173,7 +161,6 @@ function readPages(site) {
       let name = path.basename(stripExtOfPathname(page.filename));
       page.slug = slug(normalize(name));
     }
-    page.content = convertCommonmarkToHtml(page.content);
     if (!page.url) {
       let dirname = path.dirname(page.filename);
       let full_url = path.join(dirname, page.slug + site.writeExt);
@@ -191,18 +178,16 @@ function readPages(site) {
   }
 
   if (nSkip) {
-    console.log(`readPages no changes ${nSkip} files`)
+    console.log(`readPages no changes ${nSkip} files`);
   }
-
 }
-
 
 function generateIndexableSubpages(site) {
   for (let page of site.pages) {
     if (page.index) {
       let subpages = _.omitBy(site.pages, { index: true });
       if (page.category) {
-        subpages = _.filter(subpages, {category: page.category})
+        subpages = _.filter(subpages, { category: page.category });
       }
       if (page.sortKey) {
         subpages = _.sortBy(subpages, page.sortKey);
@@ -220,12 +205,11 @@ function generateIndexableSubpages(site) {
   }
 }
 
-
 function getTemplateDir(page, site) {
   let templateDirs = [
     path.dirname(page.filename),
     site.templateDir,
-    path.join(__dirname, 'defaults')
+    path.join(__dirname, "defaults")
   ];
 
   for (let templateDir of templateDirs) {
@@ -234,9 +218,8 @@ function getTemplateDir(page, site) {
       return template;
     }
   }
-  return null
+  return null;
 }
-
 
 function writePages(site) {
   let nSkip = 0;
@@ -267,9 +250,9 @@ function writePages(site) {
 
     let text = page.content;
 
-    let metadata = {page, site};
+    let metadata = { page, site };
 
-    if (_.endsWith(template, 'pug')) {
+    if (_.endsWith(template, "pug")) {
       let compileWithMetadata = pug.compileFile(template, { pretty: true });
       text = compileWithMetadata(metadata);
     }
@@ -281,15 +264,14 @@ function writePages(site) {
   }
 
   if (nSkip) {
-    console.log(`writeSiteFiles no changes ${nSkip} files`)
+    console.log(`writeSiteFiles no changes ${nSkip} files`);
   }
 }
 
-
 function processSite(site) {
-  if (!site.forced &&isFile(site.cachedPages)) {
+  if (!site.forced && isFile(site.cachedPages)) {
     console.log(`processSite cachedPages ${site.cachedPages}`);
-    let text = fs.readFileSync(site.cachedPages, 'utf8');
+    let text = fs.readFileSync(site.cachedPages, "utf8");
     site.pages = _.concat(site.pages, JSON.parse(text));
   }
 
@@ -300,42 +282,40 @@ function processSite(site) {
 
   if (site.cachedPages) {
     console.log(`processSite saveCachedPages ${site.cachedPages}`);
-    _.map(site.pages, (page) => { page.subpages = []; });
+    _.map(site.pages, page => {
+      page.subpages = [];
+    });
     fs.writeFileSync(site.cachedPages, JSON.stringify(site.pages));
   }
 }
 
-
 function watchSite(site) {
-  let finalhandler = require('finalhandler');
-  let serveStatic = require('serve-static');
+  let finalhandler = require("finalhandler");
+  let serveStatic = require("serve-static");
   let serve = serveStatic(site.outputDir);
 
-  require('http')
-    .createServer(
-      (req, res) => serve(req, res, finalhandler(req, res)))
+  require("http")
+    .createServer((req, res) => serve(req, res, finalhandler(req, res)))
     .listen(8000);
 
-  console.log('Serving on http://localhost:8000')
+  console.log("Serving on http://localhost:8000");
 
-  require("watch").watchTree(
-    site.contentDir, (f) => processSite(site));
+  require("watch").watchTree(site.contentDir, f => processSite(site));
 
   console.log(`Watching files in ${site.contentDir}`);
 }
-
 
 function parseOptions() {
   let knownOpts = {
     site: [String, null],
     recursive: Boolean,
-    watch: Boolean,
+    watch: Boolean
   };
 
   let shortHands = {
     r: ["--recursive"],
     s: ["--site"],
-    w: ["--watch"],
+    w: ["--watch"]
   };
 
   let parsed = nopt(knownOpts, shortHands, process.argv, 2);
@@ -348,31 +328,29 @@ function parseOptions() {
   return parsed;
 }
 
-
 if (require.main === module) {
-
   let parsed = parseOptions();
 
   let site = {
-    url: '', // if '' then use relative urls
-    outputDir: '.',  // generated files and static files put here
+    url: "", // if '' then use relative urls
+    outputDir: ".", // generated files and static files put here
     files: [], // list of all files to be converted
-    contentDir: '',  // look for markdown files
-    pages: [],  // stores all the processed pages found in 'content_dir'
-    templateDir: '.',  // look for templates
-    mediaDir: '.',  // files to be copied directly into the output directory
-    cachedPages: '',  // if not empty, caching file to spend updates
-    readExts: ['.md', '.txt', '.mkd', '.markdown'],
-    writeExt: '.html', // extensions to converted html
-    dateFormatString: "fullDate", // the formatting of page.date 
+    contentDir: "", // look for markdown files
+    pages: [], // stores all the processed pages found in 'content_dir'
+    templateDir: ".", // look for templates
+    mediaDir: ".", // files to be copied directly into the output directory
+    cachedPages: "", // if not empty, caching file to spend updates
+    readExts: [".md", ".txt", ".mkd", ".markdown"],
+    writeExt: ".html", // extensions to converted html
+    dateFormatString: "fullDate", // the formatting of page.date
     recursive: false, // search all subdirectories in contentDir
-    force: false, // don't use cache, process all files
+    force: false // don't use cache, process all files
   };
 
   if (parsed.site) {
     console.log(`Load site config ${parsed.site}`);
-    let text = fs.readFileSync(parsed.site, 'utf8')
-    _.assign(site, yaml.safeLoad(text))
+    let text = fs.readFileSync(parsed.site, "utf8");
+    _.assign(site, yaml.safeLoad(text));
   }
 
   if (!_.isUndefined(parsed.recursive)) {
@@ -387,7 +365,7 @@ if (require.main === module) {
       site.files.push(file);
     }
     if (isDirectory(file)) {
-      let files = getFiles(file, site.readExts, site.recursive)
+      let files = getFiles(file, site.readExts, site.recursive);
       site.files = _.concat(site.files, files);
     }
   }
@@ -397,9 +375,4 @@ if (require.main === module) {
   } else {
     processSite(site);
   }
-
 }
-
-
-
-
